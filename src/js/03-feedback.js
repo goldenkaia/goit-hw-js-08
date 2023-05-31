fb7426f88409abe7e2667ddf2526e359b2dc7de1
goldenkaia <julia_tserkovna@gmx.net>
2023-05-31 21:09:38 +0200

    homework 3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+const formEl = document.querySelector('.feedback-form');
 
+formEl.addEventListener('submit', onSubmit);
+formEl.addEventListener('input', onInput);
+window.addEventListener('load', onLoad);
+
+let formData = {};
+const STORAGE_KEY = 'item';
+
+function onSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  console.log(formData);
+  formData = {};
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value.trim();
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onLoad() {
+  try {
+    const data = localStorage.getItem(STORAGE_KEY);
+    if (!data) return;
+    formData = JSON.parse(data);
+    Object.entries(formData).forEach(([key, val]) => {
+      formEl.elements[key].value = val;
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+}
